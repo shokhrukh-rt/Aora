0c89c1e2a27b66680d37b7792e68055dca7b776d
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2024-05-16 17:04:00 +0900

    Initial commit

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Aora</Text>
+			<StatusBar style="auto" />
+			<Link href="/profile" style={{ color: "blue" }}>
+				Go to Profile
+			</Link>
+		</View>
+	);
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
