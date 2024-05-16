b2b563071eb11f16eeb50dc36c09649e4f14fcc2
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2024-05-16 18:18:03 +0900

    created navigation bar

--- app/index.jsx
+++ app/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "expo-router";
 const App = () => {
 	return (
 		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-3xl">Aora</Text>
+			<Text className="text-3xl font-pblack">Aora</Text>
 			<StatusBar style="auto" />
-			<Link href="/profile" style={{ color: "blue" }}>
-				Go to Profile!
+			<Link href="/home" style={{ color: "blue" }}>
+				Go to Home!
 			</Link>
 		</View>
 	);
